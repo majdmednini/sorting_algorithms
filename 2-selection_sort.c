eda60eef772c18e5e43fb719c31ee4161eda549d
threadsv1 <majdimed3@gmail.com>
2022-02-17 00:32:06 +0100

    Hot Fix

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,38 +1,54 @@
-#include"sort.h"
-void swap(int *xp, int *yp);
+#include "sort.h"
+
 /**
- * swap - function that swapto number.
- * @xp: first nmber.
- * @yp: second number.
+ * sort_min - search for the min in a table and place it
+ * in the beginning of the array
+ * @array: the given array
+ * @start: starting point of the table
+ * @size: size of the array
+ * Return:  1 or 0
  */
 
-void swap(int *xp, int *yp)
+size_t sort_min(int *array, size_t start, size_t size)
 {
-	int temp = *xp;
-	*xp = *yp;
-	*yp = temp;
+	size_t i = start + 1, x = start, sorted = 1;
+	int  min = array[start];
+
+	for (; i < size; i++)
+	{
+		if (min > array[i])
+		{
+			min = array[i];
+			x = i;
+			sorted = 0;
+		}
+	}
+	array[x] = array[start];
+	array[start] = min;
+	return (sorted);
 }
+
 /**
- * selection_sort - function that sorts an array  using the Selection sort.
- * @array: table of array.
- * @size: size of array.
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ * @array: the array to sort
+ * @size: the size of the array
+ * Return: void
  */
+
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
+	size_t i = 0;
+	size_t sorted = 0;
 
-	for (i = 0; i < size; i++)
+	if (size < 2)
+		return;
+
+	while (i < size)
 	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
-			if (array[j] < array[min_idx])
-			{
-				min_idx = j;
-			}
-		if (min_idx != i)
-		{
-			swap(&array[min_idx], &array[i]);
+		sorted = sort_min(array, i, size);
+		if (!sorted)
 			print_array(array, size);
-		}
+		i++;
 	}
 }
